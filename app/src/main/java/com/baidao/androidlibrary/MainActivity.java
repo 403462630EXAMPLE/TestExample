4b036eaa5363225d0f84c63389c541577593c038
hexi <xi.he@baidao.com>
2016-01-22 15:24:48 +0800

    init AndroidLibrary

---
+++ app/src/main/java/com/baidao/androidlibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.baidao.androidlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hexi on 16/1/22.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
