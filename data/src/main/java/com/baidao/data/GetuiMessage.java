c8e8092053dac839fbae7c3433e2dc705aaa74e5
hexi <xi.he@baidao.com>
2016-01-28 16:57:42 +0800

    Add module notification

--- data/src/main/java/com/baidao/data/GetuiMessage.java
+++ data/src/main/java/com/baidao/data/GetuiMessage.java
@@ -1,5 +1,8 @@
 package com.baidao.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by chengxin on 4/20/15.
  */
@@ -15,30 +18,30 @@ public class GetuiMessage {
         public String title;
     }
 
-    public class Extra {
-        public int dataType;
-        public int detailId;
-        public int contentId;
-        public int quoteId;
-        public String id;
-        public String target;
-        public String time;
-        public String sid;
-        public String klineType;
-        public String accuracy;//(上周准确率)100%
-        public String yieldRate;//(预期收益)12%
-        public String operator;//(操作人)周宏
-        public double price;
-        public int number;//数量
-        public double avgPrice;//持仓均价
-        public double profitPrice;//止盈价
-        public double lossPrice;//止损价
-        public long planId;
-    }
+//    public class Extra {
+//        public int dataType;
+//        public int detailId;
+//        public int contentId;
+//        public int quoteId;
+//        public String id;
+//        public String target;
+//        public String time;
+//        public String sid;
+//        public String klineType;
+//        public String accuracy;//(上周准确率)100%
+//        public String yieldRate;//(预期收益)12%
+//        public String operator;//(操作人)周宏
+//        public double price;
+//        public int number;//数量
+//        public double avgPrice;//持仓均价
+//        public double profitPrice;//止盈价
+//        public double lossPrice;//止损价
+//        public long planId;
+//    }
 
     public String display_type;
     public Body body;
-    public Extra extra;
+    public Map<String, String> extra = new HashMap<String, String>();
 
     public boolean isPlaySound() {
         return (body.play_sound != null && body.play_sound.equals("true")) ? true :false;
